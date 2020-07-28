b9a7646c77f365011b5b3bc0271502f171058de8
“inziani” <ValentineRobai.Inziani@gmail.com>
2020-07-28 15:44:48 +0300

    formating the page footer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-// $(document).ready(function(){
-//   $('.header').height($(window).height())
-// })
-// 
+$(document).ready(function(){
+  $(".design").click(function(){
+    $(".design_text").show();
+  });
+
+  $(".development").click(function(){
+    $(".development_text").show();
+  });
+
+  $(".prd_mgt").click(function(){
+    $(".prd_mgt_text").show();
+  });
+
+})
